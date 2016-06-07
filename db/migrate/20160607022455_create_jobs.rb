eb0fa068f9cf56a8eb53bc44430849415651f4df
yoshioka-s <yoshioka63@gmail.com>
2016-06-06 22:53:47 -0700

    fix migration

--- db/migrate/20160607022455_create_jobs.rb
+++ db/migrate/20160607022455_create_jobs.rb
@@ -3,7 +3,7 @@ class CreateJobs < ActiveRecord::Migration
     create_table :jobs do |t|
       t.string :title
       t.text :description
-      t.references :employer, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.integer :wage
 
       t.timestamps null: false
